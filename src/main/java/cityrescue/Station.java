132b3f01a8468a5e3ba05432bdacb97671a53ff7
shriAnand309 <sa1253@exeter.ac.uk>
2026-02-28 11:32:19 +0000

    Base of station
    defining the x and y coordinates
    showing the max amount of units this station can hold
    array built to store units that needed to be stored at the station

--- src/main/java/cityrescue/Station.java
+++ src/main/java/cityrescue/Station.java
@@ -1,3 +1,16 @@
 package cityrescue;
 
 public class Station { 
+
+  private int id;
+
+  private int x;
+
+  private int y;
+
+  private int capacity;
+
+  private int[] unitIds;
+
+  private int unitCount;
+  

d6538e55e0fd51ba0082a5b00bad6acbe449872a
shriAnand309 <sa1253@exeter.ac.uk>
2026-02-28 15:56:17 +0000

    Update PoliceCar.java
    returns police car only when the incident brought up is crime 
    returns 3 ticks because solving a crime uses up 3 ticks

--- src/main/java/cityrescue/PoliceCar.java
+++ src/main/java/cityrescue/PoliceCar.java
@@ -4,7 +4,20 @@ import cityresuce.enums.*;
 public class PoliceCar extends Unit {
 
   public PoliceCar(int id, int homeStationId, int x, int y) {
+
+    
     super(id, UnitType.POLICE_CAR, homeStationId, x, y);
+    
+  }
+  
+
+  @Override
+  public boolean canHandle(IncidentType type) {
+
+    return type == IncidentType.CRIME;
   }
 
-  
+  @Override
+  public int getTicksToResolve() {
+    return 3;
+  }

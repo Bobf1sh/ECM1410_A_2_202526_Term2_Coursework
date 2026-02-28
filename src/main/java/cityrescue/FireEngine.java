2a343216366666212a1893ca8317df7dacbc7657
shriAnand309 <sa1253@exeter.ac.uk>
2026-02-28 15:40:52 +0000

    initialise fire engine
    set the firengine to handle all and only FIRE problems in the city
    give it its own unit and station ID and its starting x/y coordinates 
    identifites the unit classification

--- src/main/java/cityrescue/FireEngine.java
+++ src/main/java/cityrescue/FireEngine.java
@@ -1,1 +1,18 @@
+package cityrescue;
 
+import cityrescue.enums.*;
+
+public class FireEngine extends Unit {
+
+  
+  public FireEngine(int id, int homeStationId, int x, int y) {
+
+    super( id, UnitType.FIRE_ENGINE, homeStationId, x, y);
+  }
+
+   
+  @Override
+  public boolean canHandle(IncidentType type) {
+
+  return type == IncidentType.FIRE;
+  }

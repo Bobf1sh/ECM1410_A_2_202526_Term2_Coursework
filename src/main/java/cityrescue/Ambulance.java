dc88ed468d3d89bad13fe9c561a82f41428e3319
shriAnand309 <sa1253@exeter.ac.uk>
2026-02-28 16:05:14 +0000

    Update Ambulance.java
    returns 2 ticks

--- src/main/java/cityrescue/Ambulance.java
+++ src/main/java/cityrescue/Ambulance.java
@@ -7,3 +7,13 @@ public class Ambulance extends Unit {
   public Ambulance(int id, int homeStationId, int x, int y) {
     super(id.UnitType.AMBULANCE.homeStationId, x, y);
   }
+
+  @Override
+  public boolean canHandle(IncidentType type) {
+    return type == IncidentType.MEDICAL;
+  }
+
+  @Override
+  public int getTicksToResolve() {
+    return 2;
+  }

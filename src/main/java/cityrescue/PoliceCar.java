57604fe4676292aef7c4bc67044a4d8fab941b6b
shriAnand309 <sa1253@exeter.ac.uk>
2026-02-28 15:52:38 +0000

    Update PoliceCar.java
    policecar is a subclass of Unit and it handles all and only CRIME labelled incidents
    it has its own unique unit ID
    associated home station ID and starting x/y coordinates

--- src/main/java/cityrescue/PoliceCar.java
+++ src/main/java/cityrescue/PoliceCar.java
@@ -1,1 +1,10 @@
+package cityrescue;
 
+import cityresuce.enums.*;
+public class PoliceCar extends Unit {
+
+  public PoliceCar(int id, int homeStationId, int x, int y) {
+    super(id, UnitType.POLICE_CAR, homeStationId, x, y);
+  }
+
+  

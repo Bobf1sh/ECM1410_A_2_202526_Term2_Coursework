e2843f4af39887b410f6ebf2116dcb3b4e5d5131
shriAnand309 <sa1253@exeter.ac.uk>
2026-02-28 11:38:22 +0000

    storing
    storing the x and y coordinates and setting the capacity to 1
    make the unit id equal to the capacity 
    set units to 0

--- src/main/java/cityrescue/Station.java
+++ src/main/java/cityrescue/Station.java
@@ -13,4 +13,21 @@ public class Station {
   private int[] unitIds;
 
   private int unitCount;
+
+  public Station(int id, String name, int x, int y) {
+
+    this.id = id;
+
+    this.name = name;
+
+    this.x = x;
+
+    this.y = y;
+
+    this.capacity = 1;
+
+    this.unitIds = new int[capacity];
+
+    this.unitCount = 0;
+  }
   

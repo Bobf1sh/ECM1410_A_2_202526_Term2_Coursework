229e78bccd4d6dcc7432a4670b7dbaedb11aeb39
shriAnand309 <sa1253@exeter.ac.uk>
2026-02-28 15:59:26 +0000

    Update Ambulance.java
    give the ambulance its own coordinates, station id, unit id
    ensure it takes care of MEDICAL incidents only in the city

--- src/main/java/cityrescue/Ambulance.java
+++ src/main/java/cityrescue/Ambulance.java
@@ -1,1 +1,9 @@
 
+package cityrescue;
+
+import cityrescue.enums.*;
+
+public class Ambulance extends Unit {
+  public Ambulance(int id, int homeStationId, int x, int y) {
+    super(id.UnitType.AMBULANCE.homeStationId, x, y);
+  }

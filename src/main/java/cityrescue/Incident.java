8ee05e1505f83b8f8b1cb31ead7900d21f88064c
shriAnand309 <sa1253@exeter.ac.uk>
2026-02-28 16:14:54 +0000

    Update Incident.java
    sets the x, y oordinated and the severity of it and what type of incident it is

--- src/main/java/cityrescue/Incident.java
+++ src/main/java/cityrescue/Incident.java
@@ -4,4 +4,17 @@ package cityrescue;
 import cityrescue.enums.*;
 
 public class Incident {
+
+  private int id;
+
+  private IncidentType type;
+
+  private int severity;
+
+  private int x;
+
+  private int y;
+
+  private IncidentStatus status;
+  
   

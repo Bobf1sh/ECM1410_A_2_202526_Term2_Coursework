db81cad2c3b69148e0e0688844e4110c36c97807
shriAnand309 <sa1253@exeter.ac.uk>
2026-02-28 15:07:42 +0000

    returns and updates
    returns unit id, home station, return x and y coordinates, and the current status, incident id, how many things remaining, operational status and the unit operations on the grid

--- src/main/java/cityrescue/Unit.java
+++ src/main/java/cityrescue/Unit.java
@@ -17,4 +17,56 @@ public abstract class Unit {
 
   protected int workRemaining;
 
-  
+  public Unit(int id, UnitType type, int homeStationId, int x, int y) {
+    this.id = id;
+
+    this.type = type;
+    
+    this.homeStationId = homeStationId;
+
+    this.x = x;
+
+    this.y = y;
+
+    this.status = UnitStatus.IDLE;
+
+    this.assignedIncidentId = -1;
+
+    this.workRemaining = 0;
+  }
+  public int getId() {
+    return id;
+  }
+
+  public int getHomeStationId() {
+    return homeStationId;
+  }
+
+  public in getY() {
+    return y;
+  }
+
+  public UnitStatus getStatus() {
+    return status;
+  }
+
+  public int getAssignedIncidentId() {
+    return assignedIncidentId;
+  }
+
+  public int getWorkRemaining() {
+    return workRemaining;
+  }
+
+  public void setLocation(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public void setStatus(UnitStatus status) {
+    this.status = status;
+  }
+
+  public void setAssignedIncidentId(int id) {
+    this.assignedIncidentId = id;
+  }

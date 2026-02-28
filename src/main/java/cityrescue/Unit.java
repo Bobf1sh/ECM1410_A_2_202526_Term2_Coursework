5d8d75d874d619a06d024d6dcdd9f84afee2b95b
shriAnand309 <sa1253@exeter.ac.uk>
2026-02-28 14:38:04 +0000

    assigning
    assigning the x and y coordinates 
    emergency response unit
    helps to work out what they need whether its ambulance/fireengine/policecar

--- src/main/java/cityrescue/Unit.java
+++ src/main/java/cityrescue/Unit.java
@@ -1,1 +1,20 @@
+package cityrescue;
 
+import cityrescue.enums.*;
+
+public abstract class Unit {
+  protected int id;
+
+  protected UnitType type;
+
+  protected int homeStationId;
+
+  protected int x;
+
+  protected int y;
+
+  protected int assignedInidentId;
+
+  protected int workRemaining;
+
+  

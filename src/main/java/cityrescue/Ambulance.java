0b93058d2fea50f5a39a4ee0f068c4f17ebb1f7b
shriAnand309 <sa1253@exeter.ac.uk>
2026-03-02 14:17:19 +0000

    typo in ambulance

--- src/main/java/cityrescue/Ambulance.java
+++ src/main/java/cityrescue/Ambulance.java
@@ -5,7 +5,7 @@ import cityrescue.enums.*;
 
 public class Ambulance extends Unit {
   public Ambulance(int id, int homeStationId, int x, int y) {
-    super(id.UnitType.AMBULANCE.homeStationId, x, y);
+    super(id, UnitType.AMBULANCE.homeStationId, x, y);
   }
 
   @Override

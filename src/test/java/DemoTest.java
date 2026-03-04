e36aeaa2ae65fe7862a716551e96637f6da803a9
Bobf1sh <bobrblobfish@gmail.com>
2026-03-04 16:37:48 +0000

    Demonstration test for video

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,1 +1,35 @@
+import org.junit.jupiter.api.Test;
 
+public class DemoTest {
+
+    @Test
+    void demonstration() {
+        CityRescue rescue = new CityRescueImpl();
+
+        // Add station
+        rescue.addStation("S1", 0, 0);
+
+        // Add units
+        rescue.addUnit("S1", "U1", UnitType.AMBULANCE);
+        rescue.addUnit("S1", "U2", UnitType.AMBULANCE);
+
+        // Add incident
+        rescue.addIncident("I1", IncidentType.MEDICAL, 3, 0);
+
+        // Dispatch
+        rescue.dispatch("I1");
+
+        // Show state
+        System.out.println(rescue.getUnitStatus("U1"));
+        System.out.println(rescue.getIncidentStatus("I1"));
+
+        // Tick several times
+        rescue.tick();
+        rescue.tick();
+        rescue.tick();
+
+        // Show updated state
+        System.out.println(rescue.getUnitStatus("U1"));
+        System.out.println(rescue.getIncidentStatus("I1"));
+    }
+}
